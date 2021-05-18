f02f9a495e448f3782278fbccef76d9f8fc6e629
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-05-18 14:01:42 -0700

    setting up redux

--- syllabi/src/index.js
+++ syllabi/src/index.js
@@ -4,9 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import returnStoreAndPersistor from './store/ConfigureStore.js';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const {store} = returnStoreAndPersistor();
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ syllabi/src/store/reducers/Index.js
@@ -0,0 +1,14 @@
+import { combineReducers }  from 'redux';
+
+// const combineReducers = combineReducers({})
+
+
+const rootReducer = (state, action) => {
+    if(action.type === 'UNFETCH_USER_SUCCESS'){
+        state = undefined
+    };
+
+    return combineReducers(state, action)
+};
+
+export default rootReducer;
