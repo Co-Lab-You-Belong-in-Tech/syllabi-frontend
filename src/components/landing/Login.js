d706adf6345aba20f62453dc57acef84d6ceb176
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-05-20 11:21:54 -0700

    took out scss folder

--- src/components/landing/Login.js
+++ src/components/landing/Login.js
@@ -2,7 +2,26 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>This is Login Page</div>
+        <div className="login-container">
+            <div className="login-outer">
+                <div className="login-inner">
+                    <div className="text-container">
+                        <span>Login</span>
+                    </div>
+                    <div className="form-container">
+                        <input
+                            placeholder="email"
+                        />
+                        <input 
+                            placeholder="password"
+                        />
+                    </div>
+                    <div>
+                        <button>Continue</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 };
 
---
+++ src/hooks/Dark.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import { useLocalStorage } from "./LocalStorage.js";
+
+
+export const useDarkMode = initialValue => {
+    const [ storedValue, setStoredValue ] = useLocalStorage(
+        'darkModeToggler', initialValue);
+
+    useEffect(()=>{
+        if(storedValue === true){
+            return (document.getElementById('root').classList.add('dark-mode'))
+        } else {
+        return (document.getElementById('root').classList.remove('dark-mode'))
+        }
+    }, [storedValue]);
+    
+
+    const setValue = e => {
+        return setStoredValue(!storedValue)
+    };
+
+return[ storedValue, setValue ];
+};
