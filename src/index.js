fef7423fc00d6c5ec7d4cbea66f98a4d2593e197
jschuh23 <janelleschuh@gmail.com>
2021-05-24 14:55:27 -0400

    Added css link

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 // packages
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // style
 import './index.css';
+import './css/main.css';
 
 // components
 import App from './App';
 import returnStoreAndPersistor from './store/ConfigStore.js';
 
-
-const {store} = returnStoreAndPersistor();
+const { store } = returnStoreAndPersistor();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
