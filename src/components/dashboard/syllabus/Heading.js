eaacba4f5b4198bf5ef59133b9e5ab6da6d70ba5
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-05-27 10:38:12 -0700

    finished form styling

--- src/components/dashboard/syllabus/Heading.js
+++ src/components/dashboard/syllabus/Heading.js
@@ -5,15 +5,55 @@ const Heading = props => {
         <div id="heading-cont">
             <div id="heading-content-cont">
                 <span id="heading-header">Write in the essential information first... we'll make sure it's ADA compliant!</span>
-                <form>
-                    <input />
-                    <input />
-                    <input />
-                    <input />
-                    <input />
-                    <input />
-                    <input />
-                    <input />
+                <form id="heading-form">
+                    <div className="heading-input-label">
+                        <span className="heading-input-heading">Course Title</span>
+                        <input
+                            className="heading-input"
+                        />
+                    </div>
+                    <div className="heading-input-label">
+                        <span className="heading-input-heading">Course Subtitle</span>
+                        <input
+                            className="heading-input"
+                        />
+                    </div>
+                    <div className="heading-input-label">
+                        <span className="heading-input-heading">Date and Time</span>
+                        <input
+                            className="heading-input"
+                        />
+                    </div>
+                    <div className="heading-input-label">
+                        <span className="heading-input-heading">Professor Name</span>
+                        <input
+                            className="heading-input"
+                        />
+                    </div>
+                    <div className="heading-input-label">
+                        <span className="heading-input-heading">Professor Email</span>
+                        <input
+                            className="heading-input"
+                        />
+                    </div>
+                    <div className="heading-input-label">
+                        <span className="heading-input-heading">Professor Phone Number</span>
+                        <input
+                            className="heading-input"
+                        />
+                    </div>
+                    <div className="heading-input-label">
+                        <span className="heading-input-heading">Location</span>
+                        <input
+                            className="heading-input"
+                        />
+                    </div>
+                    <div className="heading-input-label">
+                        <span className="heading-input-heading">Office Hours</span>
+                        <input
+                            className="heading-input"
+                        />
+                    </div>
                     <button onClick={() => {
                         props.setCurrent('component')
                     }}>Next</button>
