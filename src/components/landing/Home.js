dcaaa5aec475cebb57997bf7417377962c66c07c
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-05-28 11:48:11 -0700

    finished landing page testimonial structure

--- src/components/landing/Home.js
+++ src/components/landing/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div id="landing-page-cont">
+            
             <div id="landing-getstarted-cont">
                 <div id="getstarted-content-cont">
                     <div id="getstarted-content">
@@ -13,7 +14,21 @@ const Home = () => {
                 </div>
                 <div id="getstarted-images"></div>
             </div>
-            <div id="landing-testimonal-cont"></div>
+            
+            <div id="landing-testimonal-cont">
+                <div id="testimonial-content-cont">
+                    <div id="testimonial-bubble"></div>
+                    <div id="testimonial-triangle"></div>
+                    <div id="testimonial-person-cont">
+                        <div id="testimonial-person-img"></div>
+                        <div id="testimonial-person-info">
+                            <span>Jane Smith</span>
+                            <span>Professor of English,</span>
+                            <span>Laney College</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div id="landing-how-cont"></div>
         </div>
     )
