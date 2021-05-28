43f78e77e20fd7b449ff1e866636ab83fd916934
jschuh23 <janelleschuh@gmail.com>
2021-05-28 15:00:41 -0400

    Removed inline styles

--- src/components/dashboard/syllabus/NewSyllabus.js
+++ src/components/dashboard/syllabus/NewSyllabus.js
@@ -1,35 +1,41 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Heading from './Heading';
 
 const NewSyallbus = () => {
-    const [current, setCurrent] = useState('heading')
+    const [current, setCurrent] = useState('heading');
     const [syllabus, setSyllabus] = useState({
         user: '1234',
         data: {
             heading: {
-                title: "Enter syllabus title",
-                subtitle: "",
-                date: "",
-                time: "",
-                name: "",
-                email: "",
-                number: "",
-                location: "",
-                officeHours : ""
-            }
-        }
+                title: 'Enter syllabus title',
+                subtitle: '',
+                date: '',
+                time: '',
+                name: '',
+                email: '',
+                number: '',
+                location: '',
+                officeHours: '',
+            },
+        },
     });
 
     const [download, setDownload] = useState(false);
 
     const SwitchCase = () => {
-        switch(current) {
+        switch (current) {
             case 'heading':
-                return <Heading data={syllabus.data} current={current} setCurrent={setCurrent} />
+                return (
+                    <Heading
+                        data={syllabus.data}
+                        current={current}
+                        setCurrent={setCurrent}
+                    />
+                );
             case 'component':
-                return 'this is the component'
+                return 'this is the component';
             default:
-                return <h1>Hi</h1>
+                return <h1>Hi</h1>;
         }
     };
 
@@ -37,19 +43,19 @@ const NewSyallbus = () => {
     //     setSyllabus({
     //         ...syllabus,
     //         syllabus.data: {
-                
+
     //         }
     //     })
     // }
 
     return (
         <div id="new-syllabus-cont">
-            <div id="new-syllabus-nav" style={{'backgroundColor':'linearGradient("to right", blue, rgba(255,0,0,1))'}}>
-                <div className="new-syllabus-navdivs" style={{'padding-left': '1%'}}>
+            <div id="new-syllabus-nav">
+                <div className="new-syllabus-navdivs">
                     <span className="new-syllabus-navtext">Home</span>
                 </div>
 
-                <div className="new-syllabus-navdivs" style={{'justifyContent': 'center'}}>
+                <div className="new-syllabus-navdivs">
                     <input
                         onChange={(e) => {
                             setSyllabus({
@@ -58,41 +64,54 @@ const NewSyallbus = () => {
                                     ...syllabus.data,
                                     heading: {
                                         ...syllabus.data.heading,
-                                        title: e.target.value
-                                    }
-                                }
-                            })
+                                        title: e.target.value,
+                                    },
+                                },
+                            });
                         }}
                         id="new-syllabus-titleform"
                         value={syllabus.data.heading.title}
                     />
                 </div>
 
-                <div className="new-syllabus-navdivs" style={{'padding-right': '1%', 'justifyContent': 'flex-end'}}>
+                <div className="new-syllabus-navdivs">
                     <div id="new-syllabus-downloadcont">
-                        <span className="new-syllabus-navtext">See Preview</span>
+                        <span className="new-syllabus-navtext">
+                            See Preview
+                        </span>
                         <div className="dropdown">
-                        <button className="new-syllabus-download" onClick={() => {
-                            setDownload(!download)
-                        }}>Download</button>
-                        {download && 
-                            <div className="dropdown-content">
-                            <div className="new-syllabus-dropdown">
-                                <div id="download-h6"><span style={{'fontSize': '12px'}}>Select File Type</span></div>
-                                <div className="dropdown-choice">Word Doc (.docx)</div>
-                                <div><button className="new-syllabus-download">Download</button></div>
-                            </div>
-                            </div>
-                        }
+                            <button
+                                className="new-syllabus-download"
+                                onClick={() => {
+                                    setDownload(!download);
+                                }}
+                            >
+                                Download
+                            </button>
+                            {download && (
+                                <div className="dropdown-content">
+                                    <div className="new-syllabus-dropdown">
+                                        <div id="download-h6">
+                                            <span>Select File Type</span>
+                                        </div>
+                                        <div className="dropdown-choice">
+                                            Word Doc (.docx)
+                                        </div>
+                                        <div>
+                                            <button className="new-syllabus-download">
+                                                Download
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
             </div>
             <SwitchCase />
         </div>
-    )
-
-
+    );
 };
 
 export default NewSyallbus;
