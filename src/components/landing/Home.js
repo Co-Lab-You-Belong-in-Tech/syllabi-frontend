f291314e6169e425f613ecf14798f43d0ec3923a
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-05-28 11:41:49 -0700

    get Started container finished

--- src/components/landing/Home.js
+++ src/components/landing/Home.js
@@ -3,7 +3,16 @@ import React from 'react';
 const Home = () => {
     return (
         <div id="landing-page-cont">
-            <div id="landing-getstarted-cont"></div>
+            <div id="landing-getstarted-cont">
+                <div id="getstarted-content-cont">
+                    <div id="getstarted-content">
+                        <span>Make your syllabus ADA compliant</span>
+                        <span>Use our tool to make an inclusive syllabus in minutes!</span>
+                        <div id="getstarted-btn-cont"><button>Get Started</button></div>
+                    </div>
+                </div>
+                <div id="getstarted-images"></div>
+            </div>
             <div id="landing-testimonal-cont"></div>
             <div id="landing-how-cont"></div>
         </div>
