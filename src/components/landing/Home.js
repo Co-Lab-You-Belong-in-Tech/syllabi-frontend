d815607cbb3c75ccc19f0724ea531fb3f6f221bb
jschuh23 <janelleschuh@gmail.com>
2021-05-29 11:44:12 -0400

    Revised structure
    Added semantic tags, reorganized layout

--- src/components/landing/Home.js
+++ src/components/landing/Home.js
@@ -1,109 +1,135 @@
 import React from 'react';
+import mainImage from '../../img/Plage-braille.jpg';
+import headShot from '../../img/testimonial-headshot.jpg';
 
 const Home = () => {
     return (
         <div id="landing-page-cont">
-            
-            <div id="landing-getstarted-cont">
+            {/* Hero section */}
+            <header id="landing-getstarted-cont" className="container">
                 <div id="getstarted-content-cont">
                     <div id="getstarted-content">
-                        <span>Make your syllabus ADA compliant</span>
-                        <span>Use our tool to make an inclusive syllabus in minutes!</span>
-                        <div id="getstarted-btn-cont"><button>Get Started</button></div>
+                        <h1>Make your syllabus ADA compliant</h1>
+                        <p>
+                            Use our tool to make an inclusive syllabus in
+                            minutes!
+                        </p>
+                        <button id="getstarted-btn-cont">Get Started</button>
                     </div>
                 </div>
-                <div id="getstarted-images"></div>
-            </div>
-            
-            <div id="landing-testimonal-cont">
-                <div id="testimonial-content-cont">
-                    <div id="testimonial-bubble"></div>
-                    <div id="testimonial-triangle"></div>
-                    <div id="testimonial-person-cont">
-                        <div id="testimonial-person-img"></div>
-                        <div id="testimonial-person-info">
-                            <span>Jane Smith</span>
-                            <span>Professor of English,</span>
-                            <span>Laney College</span>
-                        </div>
-                    </div>
+                <div id="getstarted-images">
+                    <img src={mainImage} alt="Braille Terminal Image"></img>
                 </div>
-            </div>
+            </header>
 
-            <div id="landing-how-cont">
-                <div id="how-heading-cont">
-                    <span>How it works</span>
-                </div>
-                <div id="how-subheading-cont">
-                    <span>Follow a few simple steps to make an ADA compliant syllabus,</span>
-                    <span>and an inclusive classroom</span>
-                </div>
-                <div id="how-steps-cont">
-                    <div id="how-steps-innercont">
-                        <div id="how-steps-leftcont">
-                            <div className="top-step-cont">
-                                <div className="how-step-img"></div>
-                                <div className="how-step-header">
-                                    <span>Enter your syllabus</span>
-                                    <span>essentials</span>
-                                </div>
-                                <div className="how-step-info">
-                                    <span>Folllw the prompts on our builder page!</span>
-                                </div>
+            {/* Testimonial section */}
+            <main>
+                <section id="landing-testimonal-cont">
+                    <div id="testimonial-content-cont" className="container">
+                        <div id="testimonial-bubble">
+                            <p>
+                                I care about creating an inclusive classroom,
+                                and hope all my materials can meet the needs of
+                                my students. But making my documents ADA
+                                compliant, was overwhelming and highly
+                                technical. This website has saved me hours of
+                                frustration.
+                            </p>
+                        </div>
+                        <div id="testimonial-triangle"></div>
+                        <div id="testimonial-person-cont">
+                            <div id="testimonial-person-img">
+                                <img
+                                    src={headShot}
+                                    alt="Testimonial Image"
+                                ></img>
                             </div>
-                            <div className="btm-step-cont">
-                                <div className="how-step-img"></div>
-                                <div className="how-step-header">
-                                    <span>Download</span>
-                                </div>
-                                <div className="how-step-info">
-                                    <span>Choose the file format you want and click</span>
-                                    <span>download!</span>
-                                </div>
+                            <div id="testimonial-person-info">
+                                <span>Jane Smith</span>
+                                <span>Professor of English,</span>
+                                <span>Laney College</span>
                             </div>
                         </div>
-                        
-                        <div id="how-steps-midcont">
-                            <div className="top-step-cont">
+                    </div>
+                </section>
+                {/* Instructional section */}
+                <section id="landing-how-cont" className="container">
+                    <div id="how-heading-cont">
+                        <h2>How it works</h2>
+                        <p>
+                            Follow a few simple steps to make an ADA compliant
+                            syllabus, and an inclusive classroom!
+                        </p>
+                    </div>
+                    <div id="how-steps-cont">
+                        <div id="how-steps-innercont">
+                            <div className="how-steps-box">
                                 <div className="how-step-img"></div>
                                 <div className="how-step-header">
-                                    <span>Add components to</span>
-                                    <span>customize it</span>
+                                    <h3>Enter your syllabus essentials</h3>
                                 </div>
                                 <div className="how-step-info">
-                                    <span>Add text blocks and images as you need, in</span>
-                                    <span>the order you need</span>
+                                    <p>
+                                        Follow the prompts on our builder page!
+                                    </p>
                                 </div>
                             </div>
-                        </div>
-
-                        <div id="how-steps-rightcont">
-                            <div className="top-step-cont">
+                            <div className="how-steps-box">
                                 <div className="how-step-img"></div>
                                 <div className="how-step-header">
-                                    <span>Save</span>
+                                    <h3>Download</h3>
                                 </div>
                                 <div className="how-step-info">
-                                    <span>Our builder will autosave as you work. Save</span>
-                                    <span> your draft and come back to it later.</span>
+                                    <p>
+                                        Choose the file format you want and
+                                        click download!
+                                    </p>
                                 </div>
                             </div>
-                            <div className="btm-step-cont">
+                            <div className="how-steps-box">
                                 <div className="how-step-img"></div>
                                 <div className="how-step-header">
-                                    <span>Come back later</span>
+                                    <h3>Add components to customize it</h3>
                                 </div>
                                 <div className="how-step-info">
-                                    <span>Teaching the same course next semester? No</span>
-                                    <span>problem, make edits and download again</span>
+                                    <p>
+                                        Add text blocks and images as you need,
+                                        in the order you need.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="how-steps-box">
+                                <div className="how-step-img"></div>
+                                <div className="how-step-header">
+                                    <h3>Save</h3>
+                                </div>
+                                <div className="how-step-info">
+                                    <p>
+                                        Our builder will autosave as you work.
+                                        Save your draft and come back to it
+                                        later.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="how-steps-box">
+                                <div className="how-step-img"></div>
+                                <div className="how-step-header">
+                                    <h3>Come back later</h3>
+                                </div>
+                                <div className="how-step-info">
+                                    <p>
+                                        Teaching the same course next semester?
+                                        No problem, make edits and download
+                                        again.
+                                    </p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                </section>
+            </main>
         </div>
-    )
+    );
 };
 
 export default Home;
