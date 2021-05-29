62148dee5a881d7b58128052c3b36d0b926528d6
jschuh23 <janelleschuh@gmail.com>
2021-05-29 11:43:11 -0400

    Revised structure
    Added nav tag, added Contact link

--- src/components/landing/Nav.js
+++ src/components/landing/Nav.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
-// const Nav = () => {
-//     return (
-//         <div>
-//             <Link to="/"><div> Inclusive Syllabus</div></Link>
-
-//             <div>
-//                 <Link to="/login">Login</Link>
-//                 <Link to="/about">About</Link>
-//             </div>
-//         </div>
-//     );
-// };
-
-
 const Nav = () => {
     return (
-        <div>
-        </div>
+        <nav className="container">
+            <Link to="/">
+                <div> Inclusive Syllabus</div>
+            </Link>
+
+            <div>
+                <Link to="/login">Login</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
+        </nav>
     );
 };
+
+// const Nav = () => {
+//     return <div></div>;
+// };
 
 export default Nav;
