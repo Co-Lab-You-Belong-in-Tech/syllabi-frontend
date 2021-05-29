44c3b39d39d953acd7002a82c2b92fad845afeca
jschuh23 <janelleschuh@gmail.com>
2021-05-29 11:54:59 -0400

    Added class names

--- src/components/landing/Nav.js
+++ src/components/landing/Nav.js
@@ -3,12 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => {
     return (
-        <nav className="container">
-            <Link to="/">
-                <div> Inclusive Syllabus</div>
-            </Link>
-
-            <div>
+        <nav className="main-nav container">
+            <div className="logo">
+                <Link to="/">Inclusive Syllabus</Link>
+            </div>
+            <div className="nav-links">
                 <Link to="/login">Login</Link>
                 <Link to="/about">About</Link>
                 <Link to="/contact">Contact</Link>
